69c7d0b4f6aa83cec558395a5d0ef1f90fecae6e
Johan Ramirez <johand200505@gmail.com>
2026-07-29 15:28:04 -0400

    Created args for the SineWave

--- Lab15_jramirez47-1.py
+++ Lab15_jramirez47-1.py
@@ -1,5 +1,10 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+import math
 
 class SineWave:
-    def __init__(self):
+    def __init__(self, num_points=500, wcycles=2):
+        self.num_points = num_points
+        self.wcycles = wcycles
+
+    def gen_values(self):
         pass
